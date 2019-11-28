9a4bfac8b02125564f427ad94455ec71d257b210
matnij-4 <hej.ma5ttias@gmail.com>
2019-11-28 11:30:40 +0100

    Update hash_test.py

--- hash_test.py
+++ hash_test.py
@@ -4,10 +4,12 @@ import time
 class HashTable_probe(object):
 
     def __init__(self):
-        self.max_length = 20
+        self.max_length = 40000
         self.max_load_factor = 0.75
         self.length = 0
         self.table = [None] * self.max_length
+        self.collNum = 0
+        self.collJump = 0
 
 
     def __len__(self):
@@ -16,21 +18,21 @@ class HashTable_probe(object):
     def __setitem__(self, key, value):
         self.length += 1
         hashed_key = self._hash(key)
-
-
+        flagCol = False
+        
         while self.table[hashed_key] is not None:
+            self.collJump = self.collJump + 1
             flagCol = True
             if self.table[hashed_key][0] == key:
                 self.length -= 1
                 break
             hashed_key = self._increment_key(hashed_key)
 
-
+        if flagCol:
+            self.collNum = self.collNum + 1
         tuple = (key, value)
         self.table[hashed_key] = tuple
 
-#        if self.length / float(self.max_length) >= self.max_load_factor:
-#            self._resize()
 
     def __getitem__(self, key):
         index = self._find_item(key)
@@ -42,9 +44,7 @@ class HashTable_probe(object):
 
     def _hash(self, key):
         return key % self.max_length
-
-        # TODO more robust
-        # return hash(key) % self.max_length
+    
 
     def _increment_key(self, key):
         return (key + 1) % self.max_length
@@ -81,11 +81,13 @@ class HashTable_probe(object):
 class HashTable_prob_var(object):
 
     def __init__(self):
-        self.max_length = 20
+        self.max_length = 40000
         self.max_load_factor = 0.75
         self.length = 0
         self.table = [None] * self.max_length
         self.cCount = 0
+        self.collNum = 0
+        self.collJump = 0
 
     def __len__(self):
         return self.length
@@ -98,11 +100,13 @@ class HashTable_prob_var(object):
         if self.table[hashed_key] == None:
             flag = True
         else:
+            self.collNum = self.collNum + 1
             flag = self.table[hashed_key][2]
             self.table[hashed_key] = (self.table[hashed_key][0], self.table[hashed_key][1], not flag)
 
 
         while self.table[hashed_key] is not None:
+            self.collJump = self.collJump + 1
             self.cCount = self.cCount + 1
             if self.table[hashed_key][0] == key:
                 self.length -= 1
@@ -110,7 +114,7 @@ class HashTable_prob_var(object):
             hashed_key = self._increment_key(hashed_key, flag)
 
 
-            
+        
         tuple = (key, value, True)
         self.table[hashed_key] = tuple
 
@@ -165,14 +169,14 @@ class HashTable_prob_var(object):
 def randomList(n):
     randomlist = []
     for x in range(1, n+1, 1):
-        randomlist.append(random.randint(1,10000))
+        randomlist.append(random.randint(0,5))
         
     return randomlist
 
 def worstList(n):
     randomlist = []
     for x in range(1, n+1, 1):
-        randomlist.append(random.randint(1,10000))
+        randomlist.append(random.randint(1,5001))
         
     return randomlist
 
@@ -183,23 +187,21 @@ def worstList(n):
 
 # Testerna
 
-
 test_prob = HashTable_probe()
 test_var = HashTable_prob_var()
 
-randomKeys = randomList(1000000)
+randomKeys = randomList(40000)
+
 
 timestp = time.time()
-for x in range(0, 10, 1):
+for x in range(0, 1000, 1):
+    test_prob[x*40000] = x
+print(time.time() - timestp)
+
+
+timestp = time.time()
+for x in range(0, 40000, 1):
     test_var[randomKeys[x]] = x
 print(time.time() - timestp)
-
-
-timestp = time.time()
-for x in range(0, 10, 1):
-    test_prob[randomKeys[x]] = x
-print(time.time() - timestp)
-
-
 
 
